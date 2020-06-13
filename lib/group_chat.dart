ce548ec644b42a1551fe6274654e3bb16be19125
Yu Sun <yu.sun.cs@gmail.com>
2020-06-13 16:26:16 -0700

    update

--- lib/group_chat.dart
+++ lib/group_chat.dart
@@ -1,4 +1,9 @@
+import 'dart:async';
+
+import 'package:firebase_database/firebase_database.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:quiver/async.dart';
 
 class GroupChatPage extends StatefulWidget {
   @override
@@ -6,6 +11,47 @@ class GroupChatPage extends StatefulWidget {
 }
 
 class _GroupChatPageState extends State<GroupChatPage> {
+
+  var messageController = TextEditingController();
+  var scrollController = ScrollController();
+
+  var messageList = [];
+
+  _GroupChatPageState() {
+    refreshMessages();
+
+    FirebaseDatabase.instance.reference().child("messages").onChildAdded.listen((event) {
+      print("New messaged is added!");
+      refreshMessages();
+    });
+  }
+
+  void refreshMessages() {
+    FirebaseDatabase.instance.reference().child("messages").once()
+        .then((ds) {
+      print(ds.key);
+      print(ds.value);
+      var tempMsgList = [];
+      ds.value.forEach((k, v) {
+        tempMsgList.add(v);
+      });
+
+      tempMsgList.sort((a, b) {
+        return a['timestamp'].compareTo(b['timestamp']);
+      });
+
+      messageList = tempMsgList;
+      setState(() {
+        Timer(
+          Duration(seconds: 1),
+              () => scrollController.jumpTo(scrollController.position.maxScrollExtent),
+        );
+      });
+    }).catchError((error) {
+
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,13 +60,14 @@ class _GroupChatPageState extends State<GroupChatPage> {
           children: [
             Expanded(
               child: ListView.builder(
-                  itemCount: 10,
+                  controller: scrollController,
+                  itemCount: messageList.length,
                   itemBuilder: (BuildContext context, int index) {
                     return Container(
                       margin: EdgeInsets.all(20),
                       child: Row(
                         children: [
-                          Text("Message"),
+                          Text(messageList[index]['message']),
                         ],
                       ),
                     );
@@ -30,6 +77,7 @@ class _GroupChatPageState extends State<GroupChatPage> {
               children: [
                 Expanded(
                   child: TextField(
+                    controller: messageController,
                     decoration: InputDecoration(
                       border: OutlineInputBorder(),
                       labelText: 'Type your message here',
@@ -38,6 +86,23 @@ class _GroupChatPageState extends State<GroupChatPage> {
                 ),
                 IconButton(
                   icon: Icon(Icons.send),
+                  onPressed: () {
+                    // form a message
+                    var timestamp = new DateTime.now().millisecondsSinceEpoch;
+                    var message = {
+                      'timestamp' : timestamp,
+                      'message' : messageController.text,
+                    };
+                    // send the message to Firebase
+                    FirebaseDatabase.instance.reference().child("messages/" + timestamp.toString())
+                        .set(message)
+                        .then((value) {
+                          print("Message sent");
+                        }).catchError((error) {
+                          print("Failed to send the message");
+                        });
+
+                  },
                 )
               ],
             )
